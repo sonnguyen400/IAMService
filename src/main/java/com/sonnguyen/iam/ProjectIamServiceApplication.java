a7cf2470b8c50a6cf552e3d7dbf558fdce24b9c0
Nguyễn Hoàng Sơn <sonnguyen9616@gmail.com>
2024-11-20 08:58:56 +0700

    Init Project

---
+++ src/main/java/com/sonnguyen/iam/ProjectIamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sonnguyen.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectIamServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonnguyen/iam/ProjectIamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonnguyen.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIamServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
